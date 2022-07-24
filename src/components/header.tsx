b056f6a29dab0e545cba68257b8d937a80af3380
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-07-24 17:41:26 -0300

    chore: add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import { Heading, HStack, useTheme } from 'native-base';
+
+type Props = {
+  title: string;
+};
+
+export const Header = ({ title }: Props) => {
+  const { colors } = useTheme();
+
+  return (
+    <>
+      <HStack
+        py={4}
+        bg={colors.blue[500]}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Heading color="white" fontSize="md">
+          {title}
+        </Heading>
+      </HStack>
+    </>
+  );
+};
