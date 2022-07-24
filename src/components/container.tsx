6be3bfe9b27eaa89f859ff6a7f8b03bec3deee7f
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-07-24 17:41:39 -0300

    chore: add container component

---
+++ src/components/container.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { useTheme, View } from 'native-base';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const Container = ({ children }: Props) => {
+  const { colors } = useTheme();
+
+  return (
+    <View bg={colors.blue[500]} flex={1}>
+      <StatusBar style="light" />
+      <SafeAreaView style={{ flex: 1 }} edges={['top']}>
+        <View bg="white" flex={1}>
+          {children}
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
