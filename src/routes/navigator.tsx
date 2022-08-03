8673212b54ce75f35fc9819537ca0d3ed62eff93
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-08-02 22:31:57 -0300

    chore: add navigation icons

--- src/routes/navigator.tsx
+++ src/routes/navigator.tsx
@@ -1,12 +1,39 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'native-base';
+import {
+  House as HouseIcon,
+  CreditCard as CardIcon,
+} from 'phosphor-react-native';
 
 import { Home } from '../screens/home';
 
 const Tab = createBottomTabNavigator();
 
 export const Navigator = () => {
+  const { colors } = useTheme();
+
+  const renderIcons = (route: string, focused: boolean) => {
+    const color = focused ? colors.blue[600] : colors.gray[300];
+
+    switch (route) {
+      case 'Home':
+        return <HouseIcon color={color} />;
+      case 'Cards':
+        return <CardIcon color={color} />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: colors.blue[600],
+        tabBarInactiveTintColor: colors.gray[300],
+        tabBarIcon: ({ focused }) => renderIcons(route.name, focused),
+      })}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Cards" component={Home} />
     </Tab.Navigator>
