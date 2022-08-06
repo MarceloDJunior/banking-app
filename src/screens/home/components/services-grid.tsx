d7726ac16e1ff29c7898bc16b1cb2e31d29e8767
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-08-06 13:50:22 -0300

    chore: update services icons style

--- src/screens/home/components/services-grid.tsx
+++ src/screens/home/components/services-grid.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, HStack, Text, useTheme, VStack } from 'native-base';
+import { Box, Button, HStack, Text, useTheme, VStack } from 'native-base';
 import {
   Money as CashInIcon,
   UploadSimple as SendIcon,
@@ -68,20 +68,29 @@ const ServiceItem = ({ icon, title }: ServiceItemType) => {
 
   return (
     <VStack alignItems="center">
-      <Button
-        rounded="full"
-        alignItems="center"
-        justifyContent="center"
-        bg="gray.100"
-        size="16"
-        _pressed={{ bg: 'gray.200' }}
-      >
-        {React.cloneElement(icon, {
-          size: 30,
-          color: colors.primary[500],
-        })}
+      <Button rounded="full" size="16">
+        <Box
+          flex={1}
+          rounded="full"
+          size="16"
+          alignItems="center"
+          justifyContent="center"
+          opacity={0.9}
+          bg={{
+            linearGradient: {
+              colors: [colors.primary[600], colors.primary[700]],
+              start: [0, 0],
+              end: [1, 0],
+            },
+          }}
+        >
+          {React.cloneElement(icon, {
+            size: 30,
+            color: colors.white,
+          })}
+        </Box>
       </Button>
-      <Text fontSize="xs" fontWeight="bold" mt={2}>
+      <Text fontSize="xs" fontWeight="medium" mt={2}>
         {title}
       </Text>
     </VStack>
@@ -96,7 +105,7 @@ export const ServicesGrid = () => {
           <ServiceItem key={item.id} {...item} />
         ))}
       </HStack>
-      <HStack width="full" justifyContent="space-evenly" mt={6}>
+      <HStack width="full" justifyContent="space-evenly" mt={4}>
         {SERVICES_LINE_2.map(item => (
           <ServiceItem key={item.id} {...item} />
         ))}
