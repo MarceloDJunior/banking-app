87f56a554365f1a8a3e18578f9a976915c82fb18
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-08-08 15:58:37 -0300

    chore: add scale animation to tab icon

--- src/navigation/bottom-tabs/components/bottom-tab-item.tsx
+++ src/navigation/bottom-tabs/components/bottom-tab-item.tsx
@@ -4,6 +4,12 @@ import {
   CreditCard as CardIcon,
   Gear as SettingsIcon,
 } from 'phosphor-react-native';
+import { useLayoutEffect } from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 
 type Props = {
   isCurrent: boolean;
@@ -11,29 +17,53 @@ type Props = {
   route: string;
 };
 
+const AnimatedVStack = Animated.createAnimatedComponent(VStack);
+
 export const BottomTabItem = ({ isCurrent, label, route }: Props) => {
   const { colors } = useTheme();
   const color = isCurrent ? colors.primary[500] : colors.gray[500];
+  const scale = useSharedValue(1);
 
   const renderIcon = () => {
     switch (route) {
       case 'Cards':
-        return <CardIcon color={color} size={30} />;
+        return <CardIcon color={color} size={24} />;
       case 'Home':
-        return <HouseIcon color={color} size={30} />;
+        return <HouseIcon color={color} size={24} />;
       case 'Settings':
-        return <SettingsIcon color={color} size={30} />;
+        return <SettingsIcon color={color} size={24} />;
       default:
         return null;
     }
   };
 
+  useLayoutEffect(() => {
+    if (isCurrent) {
+      scale.value = 1.1;
+    } else {
+      scale.value = 1;
+    }
+  }, [isCurrent, scale]);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: withSpring(scale.value) }],
+    };
+  });
+
   return (
-    <VStack w="full" alignItems="center" justifyContent="center" height="full">
+    <AnimatedVStack
+      w="full"
+      alignItems="center"
+      justifyContent="center"
+      height="full"
+      p={2}
+      style={animatedStyles}
+    >
       {renderIcon()}
       <Text fontSize="2xs" color={color}>
         {label}
       </Text>
-    </VStack>
+    </AnimatedVStack>
   );
 };
