237f3879717e94b4f1d01af53aaadc6d987a172b
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-08-09 13:37:43 -0300

    chore: update card names

--- src/constants/cards.ts
+++ src/constants/cards.ts
@@ -4,37 +4,37 @@ export const CARDS: CardModel[] = [
   {
     id: 1,
     number: '123456789',
-    name: 'John Doe',
+    name: 'Marcelo D. Junior',
     expiry: '12/20',
   },
   {
     id: 2,
     number: '111111111',
-    name: 'Jane Doe',
+    name: 'Marcelo D. Junior',
     expiry: '12/20',
   },
   {
     id: 3,
     number: '222222222',
-    name: 'Alex Doe',
+    name: 'Marcelo D. Junior',
     expiry: '12/20',
   },
   {
     id: 4,
     number: '333333333',
-    name: 'Bob Doe',
+    name: 'Marcelo D. Junior',
     expiry: '12/20',
   },
   {
     id: 5,
     number: '444444444',
-    name: 'John Doe',
+    name: 'Marcelo D. Junior',
     expiry: '12/20',
   },
   {
     id: 6,
     number: '555555555',
-    name: 'Jane Doe',
+    name: 'Marcelo D. Junior',
     expiry: '12/20',
   },
 ];
