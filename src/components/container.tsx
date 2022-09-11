488466d55fec6b76a3b66d52104871cd6ce6633d
Marcelo Dornelles Junior <marcelo_dj28@hotmail.com>
2022-09-11 20:03:22 -0300

    chore: reset top position on screen change

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,7 +1,11 @@
 import { useDrawerStatus } from '@react-navigation/drawer';
+import { useFocusEffect } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { useTheme, View } from 'native-base';
+import { useCallback } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { useStickyScrollContext } from '../contexts/sticky-scroll-context';
 
 type Props = {
   children: React.ReactNode;
@@ -10,6 +14,7 @@ type Props = {
 export const Container = ({ children }: Props) => {
   const { colors, config } = useTheme();
   const drawerStatus = useDrawerStatus();
+  const { setIsScrollingDown } = useStickyScrollContext();
 
   const getStatusBarTextColor = () => {
     if (config.initialColorMode === 'dark') {
@@ -18,11 +23,17 @@ export const Container = ({ children }: Props) => {
     return drawerStatus === 'open' ? 'light' : 'dark';
   };
 
+  useFocusEffect(
+    useCallback(() => {
+      setIsScrollingDown(false);
+    }, [setIsScrollingDown])
+  );
+
   return (
     <View bg={colors.secondary[500]} flex={1}>
       <StatusBar style={getStatusBarTextColor()} />
       <SafeAreaView style={{ flex: 1 }} edges={['top']}>
-        <View bg="white" flex={1} overflow="hidden">
+        <View bg={colors.secondary[500]} flex={1} overflow="hidden">
           {children}
         </View>
       </SafeAreaView>
